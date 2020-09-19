80f006b7183604077c246ced801a95a1228e5fdd
Joseph <josdavil@espol.edu.ec>
2020-09-19 01:56:34 -0500

    documentation

--- binary_image.py
+++ binary_image.py
@@ -1,15 +1,20 @@
 
 import numpy as np
-
 from PIL import Image
-
 import cython
 
 
 def binary_image(path,save=True,th=128):
     """
-    Binarizacion de un imagen , a parti de un path 
-    @return imagen binarizadad
+     Binarizacion of an image
+    Parameters
+    ----------
+   path : str
+         path directory
+    save : boolean
+         save image default "output.png"
+    th : int
+        thresholding
     """
     im = np.array(Image.open(path).convert('L').resize((256, 256)))
     im_bin_th = (im > th) * 255
