b682bc611fc3ceb5f60ef20c704671ad5458c4ca
Joseph <josdavil@espol.edu.ec>
2020-09-19 01:46:23 -0500

    thresholding

--- binary_image.py
+++ binary_image.py
@@ -1,4 +1,4 @@
-cimport numpy as np
+
 import numpy as np
 
 from PIL import Image
@@ -7,8 +7,13 @@ import cython
 
 
 def binary_image(path,save=True,th=128):
+    """
+    Binarizacion de un imagen , a parti de un path 
+    @return imagen binarizadad
+    """
     im = np.array(Image.open(path).convert('L').resize((256, 256)))
     im_bin_th = (im > th) * 255
     if(save):
-        Image.fromarray(np.uint8(im_bin_th)).save(save)
+        Image.fromarray(np.uint8(im_bin_th)).save("output.png")
     return im_bin_th
+
